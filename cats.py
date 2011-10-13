db2ce8bd660d6ec918ec1b434c258aece5954fb8
Philip Molloy <philip.a.molloy@gmail.com>
2011-10-13 15:32:39 -0400

    Store excluded nouns, utf8 encoding, revise header
    Add encoding declaration to start of file. Update comment header to
     conform to new formatting standard. Create list to store nouns that
     don't match regex and were previously discarded into nouns-dirty.json
     file.

--- cats.py
+++ cats.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
 #
 #   cats.py
 #
-#   Find all article names under the German noun category 
-#   on the German Wiktionary. Print output list to file.
+#   PURPOSE:
+#   Find all article names under the German noun category on the German 
+#   Wiktionary, then store list of nouns in external file.
 #
+#   EXAMPLE:
+#   e.g. list=categorymembers from http://en.wikipedia.org/w/api.php
+#
+#   TODO:
+#   - Store date and time with data
 
 import wikitools
 import re
@@ -18,12 +25,17 @@ category = wikitools.category.Category(site, title='Substantiv (Deutsch)')
 
 noun_list = category.getAllMembers(titleonly = True)
 
+dirty_list = []
+
 print "Processing list..."
 
 # Remove colons, dashes and digits from category output
 regex = re.compile('[:|-|0-9]')
-clean_list = [noun for noun in noun_list if not regex.search(noun)]
+clean_list = [noun for noun in noun_list if not regex.search(noun) else dirty_list.append[noun] ]
 
 # Store object in JSON file to be processed by nget.py
 with open('nouns.json', 'w') as store:
     json.dump(clean_list, store)
+
+with open('nouns-dirty.json', 'w') as store:
+    json.dump(dirty_list, store)
