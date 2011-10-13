4ad24c3fe136d877bdfd33c5f3d7158807334843
Philip Molloy <philip.a.molloy@gmail.com>
2011-10-13 15:41:30 -0400

    Externalize comments, patterns and analysis
    Update comment header to conform to new formatting standard. Move
     detailed pattern explanation to online documentation. Move pattern
     data structure to JSON file. Remove commented testing functions.
     Move results() to separate analysis script.

--- art.py
+++ art.py
@@ -2,54 +2,33 @@
 # -*- coding: utf-8 -*-
 #
 #   art.py
-# 
-#   TODO
-#       - In compound words, both gender and plural are governed by the last
-#          word in the compound
-#       - Nouns with dual gender
-#       - There are numerous data structures in this script. Is it better to
-#          build them like pattern_dict or reverse_pattern_dict? And then under
-#          what circumstances?
-#       - How many nouns don't match a pattern, and what nouns are they?
-#       - Can I factor out the for loops into a more generic function?
-#       - What is the total break down of the genders?
-
-#   Masculine
-#       - Nouns ending in the suffixes -ig, -ling, -or, -us
-#       - Nouns ending in -en
-#   Feminine
-#       - Words designating professions and nationalities, using the suffix -in
-#          with masculine forms
-#       - Nouns ending in the suffixes -anz, -ei, -enz, -ie, -ik, -ion, -heit,
-#          -keit, -schaft, -taet, -ung, -ur, with exceptions (e.g. die Papagei)
-#       - Most nouns ending in -e (plural -n)
-#   Neuter
-#       - Nouns ending in the suffix -tum (e.g. Christentum, Judentum, 
-#          Eigentui), with exceptions (e.g. der Reichtum, der Irrtum)
-#       - Diminutive suffixes -chen, -lein (and their dialect variations -erl,
-#          -el, -le, -li)
-#       - Nouns with the suffixes -ment, -(i)um
-#       - Most collective nouns beginning with the prefix Ge-
+#
+#   PURPOSE: 
+#   Create a data structure with nouns matched to patterns and access whether
+#   pattern could accurately guess gender of each matched noun.
+#
+#   TODO:
+#   - There are numerous data structures in this script. Is it better to
+#     build them like pattern_dict or reverse_pattern_dict? And then under
+#     what circumstances?
+#   - Can I factor out the for loops into a more generic function?
+#
 
 from __future__ import print_function
 import json
 import re
 
-pattern_dict = {'Maskulinum': ['ig$', 'ling$', 'or$', 'us$', 'en$', 'el$',
- 'ant$', 'all$', 'und$', 'ang$', 'aum$', 'ich$', 'ismus$', 'er$', 'ner$',
- 'ist$'],
- 'Femininum': ['in$', 'erin$', 'anz$', 'ei$', 'enz$', 'ie$', 'ik$', 'ion$',
- 'heit$', 'keit$', 'schaft$', 'ung$', 'ur$', 'e$', u'tät$', 'ade$', 'age$',
- 'ette$', 'ine$', 'tur$', 'isse$', 'itis$', 'ive$', 't$'],
- 'Neutrum': ['tum$', 'chen$', 'lein$', 'erl$', 'el$', 'le$', 'li$', 'ment$',
- 'ium$', 'um$', '^Ge', 'o$', 'om$', 'em$', 'ett$', 'ma$', 'nis$', 'tel$']}
+
+with open('pattern.json') as pattern_file:
+    pattern_dict = json.load(pattern_file)
+
 
 # Load noun:gender dictionary
 with open('gender.json') as gender_file:
     gender_dict = json.load(gender_file)
 
 def patterns():
-    '''Transform pattern_dict to pattern:gender for simple comparison later.'''
+    """Transform pattern_dict to pattern:gender for simple comparison later."""
     
     reverse_pattern_dict = {}
     
@@ -61,7 +40,7 @@ def patterns():
     return reverse_pattern_dict
 
 def matches(reverse_dict):
-    '''Create a series of pattern dictionaries containing matching nouns.'''
+    """Create a series of pattern dictionaries containing matching nouns."""
     
     match_dict = {}
     
@@ -75,13 +54,10 @@ def matches(reverse_dict):
         for pattern in reverse_dict.keys():
             if re.search(pattern, noun):
                 match_dict[pattern][noun] = ''
-#                noun_utf = noun.encode('utf8') # Loaded as ascii
-#                print('Matched {0} to {1}'.format(noun_utf, pattern))
-#                print(match_dict)
     return match_dict
 
 def compare(reverse_dict, match_dict):
-    '''Match pattern guess gender with real noun gender.'''
+    """Match pattern guess gender with real noun gender."""
     
     for pattern in match_dict:
         for noun in match_dict[pattern].keys():
@@ -92,34 +68,9 @@ def compare(reverse_dict, match_dict):
                 match_dict[pattern][noun] = 'Wrong' #Why is it wrong?
     return match_dict
 
-def results(compare_dict):
-    '''Print requested data from comparison.'''
-    
-    # Accuracy of a pattern
-    for pattern in compare_dict.keys():
-        total = len(compare_dict[pattern])
-        correct = 0
-        wrong = 0
-        other = 0
-        
-        for result in compare_dict[pattern].values():
-            if result == 'Correct':
-                correct += 1
-            elif result == 'Wrong':
-                wrong += 1
-            else:
-                other += 1
-        
-        accuracy = round((float(correct) / float(total)) * 100, 2)
-        print(repr(pattern).rjust(10), repr(accuracy).rjust(8), repr(correct).rjust(5), repr(wrong).rjust(5))
-
-    # Any exceptions to a pattern
-    # Nouns correctly matched by pattern
-
 reverse_dict = patterns()
 match_dict = matches(reverse_dict)
 comparison = compare(reverse_dict, match_dict)
-results(comparison)
 
-#with open('compare.json', 'wb') as store:
-#    json.dump(match_dict, store)
+with open('compare.json', 'wb') as store:
+    json.dump(match_dict, store)
