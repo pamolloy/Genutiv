f1eb7cb253452fe9497b62f144e963ed1184b6bd
Philip Molloy <philip.a.molloy@gmail.com>
2012-07-20 00:33:53 -0400

    Add top-level script environment

---
+++ genutiv.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+#
+#   genutiv.py
+#
+# Test the accuracy of patterns commonly cited in educational literature
+# to help guess the gender of German nouns.
+#
+
+from reference.wiktionary import Wiktionary
+
+if __name__ == '__main__':
+    wiki = Wiktionary()
+    wiki.populate()
+
