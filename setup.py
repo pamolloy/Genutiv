6167987b82f4e7fc63a5094918fa1a7370c5dda9
Philip Molloy <philip.a.molloy@gmail.com>
2013-05-07 21:56:28 -0400

    Update distribution based upon Distutils documentation.
    Section 2 of the Distributing Python Modules article encourages the use of __init__.py files and other variables in setup()

--- setup.py
+++ setup.py
@@ -5,17 +5,18 @@ except ImportError:
     from distutils.core import setup
 
 setup(
-    name = "Genutiv",
-    version = "0.1",
-    description = "Test accuracy of patterns to predict gender of German nouns.",
-    long_description  = open("README.md").read(),
-    url = "http://pamolloy.dyndns.org/project/genutiv/",
-    download_url = "http://github.com/pamolloy/Genutiv",
-    author = "Philip Molloy",
-    author_email = "philip.a.molloy+genutiv@gmail.com",
-    license = "",   #TODO(PM) Select a GPL-compatible license
+    name = 'Genutiv',
+    version = '0.1.0',
+    description = 'Test accuracy of patterns to predict gender of German nouns.',
+    long_description  = open('README.md').read(),
+    url = 'http://philipmolloy.com/project/genutiv/',
+    download_url = 'http://github.com/pamolloy/Genutiv',
+    author = 'Philip Molloy',
+    author_email = 'philip.a.molloy+genutiv@gmail.com',
+    license = '',   #TODO(PM) Select a GPL-compatible license
     classifiers = [],   #TODO(PM) Select classifiers from list on PyPI
-    install_requires = ["re","json","urllib2","wikitools"],
-    packages = ["genutiv"],
+    requires = ['re','json','urllib2','wikitools'],
+    packages = ['genutiv', 'genutiv.analysis', 'genutiv.reference'],
+    #package_data = []	# TODO(PM): Add package data
+    #data_files = []	# TODO(PM): Add data files
 )
-
